642be16a012d39e7bdb0ce1f30684ede606b79e1
Magda Vicente <magdavicente@Magdas-MacBook-Air.local>
2025-10-08 21:35:18 -0700

    Add UserController with GET /users endpoint

--- src/main/java/com/example/sportsbook/controller/EventController.java
+++ src/main/java/com/example/sportsbook/controller/EventController.java
@@ -1,10 +1,14 @@
 package com.example.sportsbook.controller;
 
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Map;
 
-import java.util.*;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
